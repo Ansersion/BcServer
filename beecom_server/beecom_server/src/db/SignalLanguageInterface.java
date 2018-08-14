196403847d02c680c719b0e64f4be08c2441c10b
Ansersion <ansersion@gmail.com>
2018-08-14 19:47:23 +0800

    Add signal language interface

---
+++ beecom_server/beecom_server/src/db/SignalLanguageInterface.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package db;
+
+/**
+ * @author Ansersion
+ *
+ */
+public interface SignalLanguageInterface {
+	String getChinese();
+	String getEnglish();
+	String getFrench();
+	String getRussian();
+	String getArabic();
+	String getSpanish();
+}
