c7caa0def5822b7eaa9f6dea8595fa0433bba431
Ansersion <ansersion@gmail.com>
2019-07-02 11:52:16 +0800

    Fix #4: change the size of logfile to 20MB; add pack_scripts

---
+++ pack_scripts/pack.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ $# != 1 ]; then
+echo "USAGE: $0 bc_server_jar_path"
+exit 1
+fi
+
+if [ ! -e "$1" ]; then
+echo "$1 does not exist"
+exit 1
+fi
+
+MAIN_PATH=`pwd`/`dirname $0`;
+JAR_PATH=$1
+BEECOM_PATH=..;
+TARGET_PATH=target;
+
+pushd $MAIN_PATH;
+mkdir -p $TARGET_PATH;
+rm -rf $TARGET_PATH/*
+popd
+
+cp $JAR_PATH $MAIN_PATH/$TARGET_PATH;
+
+cd $MAIN_PATH;
+cp -r $BEECOM_PATH/beecom_server/beecom_server/config $TARGET_PATH/
+cp restart.sh $TARGET_PATH/
---
+++ pack_scripts/restart.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#进程名字可修改
+cd /home/ansersion/bcServer
+CMD="/usr/bin/java -jar bc_server.jar &> /dev/null &"
+LOG_FILE="restart_log.log"
+while true ; do
+     NUM=`netstat -tnul | grep -w 8025 | grep -v grep |wc -l`
+     # echo $NUM
+     #少于1，重启进程
+     if [ "${NUM}" -lt "1" ];then
+	 echo "`date` to restart" >> $LOG_FILE
+	 /usr/bin/java -jar bc_server.jar &> /dev/null &
+	 # echo "cmd end"
+     fi
+     sleep 60s
+done
+
+exit 0
